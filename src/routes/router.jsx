4927a2ed98157e8278e24dd64b8227d3e33cd103
abubakar-arman <abubakar.arman.cse@gmail.com>
2025-11-22 04:51:59 +0600

    Add AllMovies page

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router'
 import RootLayout from '../layouts/RootLayout';
 import Home from '../pages/Home';
+import AllMovies from '../pages/AllMovies';
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,12 @@ const router = createBrowserRouter([
         index: true,
         element:
           <Home />,
+        loader: () => fetch('http://localhost:3000/movies')
+      },
+      {
+        path: '/movies',
+        element:
+          <AllMovies />,
         loader: () => fetch('http://localhost:3000/movies')
       }
     ]
