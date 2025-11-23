4e13e271fb74433f3665c9b844caa06da8b68ff1
abubakar-arman <abubakar.arman.cse@gmail.com>
2025-11-23 21:48:06 +0600

    Fix scrolling bug

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import { Outlet } from 'react-router';
+import { Outlet, ScrollRestoration } from 'react-router';
 
 const RootLayout = () => {
     return (
         <div className='bg-base-200'>
+            <ScrollRestoration />
             <Navbar />
             <Outlet />
             <Footer />
